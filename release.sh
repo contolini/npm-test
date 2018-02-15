24bd9f3a51d78969ef5ae0c7f01b4b2a20f5118e
Chris Contolini <chris@contolini.com>
2018-02-14 20:45:36 -0500

    See if we can grab the commit's author in Travis

--- release.sh
+++ release.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 set -e
 
-# If NPM env variables are set and we're in Travis
-if [[ -n "$NPM_USERNAME" ]] && [[ -n "$NPM_PASSWORD" ]] && [[ -n "$NPM_EMAIL" ]] && [[ -n "$TRAVIS" ]]; then
-  echo "Logging into npm..."
-  echo -e "${NPM_USERNAME}\n${NPM_PASSWORD}\n${NPM_EMAIL}\n" | npm login
-  echo "Adding git credentials..."
-  git config user.name "CFPBot"
-  git config user.email "CFPBot@users.noreply.github.com"
+export COMMIT_AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
+
+# If this is Travis CI AND
+if [[ -n "$TRAVIS" ]] &&
+      # This is a pull request OR 
+   [[ "$TRAVIS_PULL_REQUEST" != "false" ||
+      # The commit author is CFPBot
+      "$COMMIT_AUTHOR_NAME" == "CFPBot" ]];
+# Then echo some variables and abort everything
+then
+  echo "TRAVIS: ${TRAVIS}"
+  echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+  echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+  echo "GH_PROD_BRANCH: ${GH_PROD_BRANCH}"
+  echo "GH_DEV_BRANCH: ${GH_DEV_BRANCH}"
+  echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
+  echo "COMMIT_AUTHOR_NAME: ${COMMIT_AUTHOR_NAME}"
+  echo "Abort!"
+  exit 0;
 fi
+
+echo "TRAVIS: ${TRAVIS}"
+echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+echo "GH_PROD_BRANCH: ${GH_PROD_BRANCH}"
+echo "GH_DEV_BRANCH: ${GH_DEV_BRANCH}"
+echo "TRAVIS_NODE_VERSION: ${TRAVIS_NODE_VERSION}"
+echo "COMMIT_AUTHOR_NAME: ${COMMIT_AUTHOR_NAME}"
+echo "Success!"
